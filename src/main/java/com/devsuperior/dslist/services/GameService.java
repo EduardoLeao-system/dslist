1a01c7ed746cb6b9afc942cfdde6b58dbe420a7c
EduardoLeao-system <carloseduardomeirelesleao@gmail.com>
2025-08-19 15:06:23 -0400

    Game repository, service, controller

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
@@ -14,7 +16,8 @@ public class GameService {
 	private GameRepository gameRepository;
 	
 	public List<GameMinDTO> findAll() {
-		
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 
 }
